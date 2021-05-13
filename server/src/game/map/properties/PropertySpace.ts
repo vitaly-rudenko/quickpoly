b38b01c1e6bcf75b6c81bd7db853f75b9d1b9585
Vitaly Rudenko <maurice.dederick@gmail.com>
2021-05-13 15:21:18 +0300

    Moved residence actions to PropertySpace

--- server/src/game/map/properties/PropertySpace.ts
+++ server/src/game/map/properties/PropertySpace.ts
@@ -1,5 +1,11 @@
 import type { Player } from '../../Player';
+import type { Context } from '../../Context';
 import { Space } from '../Space';
+import { Action } from '../../actions/Action';
+import { PayPropertyRentAction } from '../../actions/PayPropertyRentAction';
+import { PurchasePropertyAction } from '../../actions/PurchasePropertyAction';
+import { PutPropertyUpForAuctionAction } from '../../actions/PutPropertyUpForAuctionAction';
+import { RollDiceAction } from '../../actions/RollDiceAction';
 
 export abstract class PropertySpace extends Space {
     protected _landlord: Player | null;
@@ -20,6 +26,34 @@ export abstract class PropertySpace extends Space {
     }
 
     abstract calculateRent(): number;
+
+    getResidenceActions(context: Context): Action[] {
+        const actions: Action[] = [];
+
+        if (context.move.hasActionBeenPerformed('rollDice')) {
+            if (this._landlord) {
+                if (
+                    this._landlord !== context.move.player &&
+                    !context.move.hasActionBeenPerformed('payPropertyRent')
+                ) {
+                    actions.push(new PayPropertyRentAction(this));
+                }
+            } else {
+                if (
+                    context.move.player.canPay(this._price) &&
+                    !context.move.hasActionBeenPerformed('purchaseProperty')
+                ) {
+                    actions.push(new PurchasePropertyAction(this));
+                }
+
+                actions.push(new PutPropertyUpForAuctionAction(this));
+            }
+        } else {
+            actions.push(new RollDiceAction());
+        }
+
+        return actions;
+    }
 
     setLandlord(player: Player): void {
         this._landlord = player;
--- server/src/game/map/properties/StreetSpace.ts
+++ server/src/game/map/properties/StreetSpace.ts
@@ -1,10 +1,4 @@
-import type { Context } from '../../Context';
 import type { Player } from '../../Player';
-import { Action } from '../../actions/Action';
-import { PayPropertyRentAction } from '../../actions/PayPropertyRentAction';
-import { PurchasePropertyAction } from '../../actions/PurchasePropertyAction';
-import { PutPropertyUpForAuctionAction } from '../../actions/PutPropertyUpForAuctionAction';
-import { RollDiceAction } from '../../actions/RollDiceAction';
 import { PropertySpace } from './PropertySpace';
 
 export class StreetSpace extends PropertySpace {
@@ -41,34 +35,6 @@ export class StreetSpace extends PropertySpace {
             : this._houses > 0
                 ? this._titleDeed.perHouseRents[this._houses]
                 : this._titleDeed.baseRent;
-    }
-
-    getResidenceActions(context: Context): Action[] {
-        const actions: Action[] = [];
-
-        if (context.move.hasActionBeenPerformed('rollDice')) {
-            if (this._landlord) {
-                if (
-                    this._landlord !== context.move.player &&
-                    !context.move.hasActionBeenPerformed('payPropertyRent')
-                ) {
-                    actions.push(new PayPropertyRentAction(this));
-                }
-            } else {
-                if (
-                    context.move.player.canPay(this._price) &&
-                    !context.move.hasActionBeenPerformed('purchaseProperty')
-                ) {
-                    actions.push(new PurchasePropertyAction(this));
-                }
-
-                actions.push(new PutPropertyUpForAuctionAction(this));
-            }
-        } else {
-            actions.push(new RollDiceAction());
-        }
-
-        return actions;
     }
 }
 
